051e7234800678a02aa09b2cd621e239045313fa
김승진 <ohksj77@gmail.com>
2024-03-26 11:44:33 +0900

    fix: type mismatch

--- deletion/src/main/java/com/soft/deletion/greeting/GreetingRepository.java
+++ deletion/src/main/java/com/soft/deletion/greeting/GreetingRepository.java
@@ -4,9 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
-import java.util.UUID;
 
-public interface GreetingRepository extends JpaRepository<Greeting, UUID> {
+public interface GreetingRepository extends JpaRepository<Greeting, Long> {
 
     @Query(value = "SELECT * FROM greeting g WHERE g.deleted_at IS NOT NULL", nativeQuery = true)
     List<Greeting> findAllByDeletedAtIsNotNull();
